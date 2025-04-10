38893a2b301a37faa98341dcac292159a031a4e1
jacobrein <jacob.rein@gabb.com>
2025-04-10 07:11:26 -0600

    - Add `yahtzee` to the score calculations list for Yahtzee scoring logic - Convert `handType` to title case when generating stats - Remove unused/legacy `YahtzeeDatabase` and related Realm database code

--- composeApp/src/nonJsMain/kotlin/com/programmersbox/fiveofakind/Database.kt
+++ composeApp/src/nonJsMain/kotlin/com/programmersbox/fiveofakind/Database.kt
@@ -1,22 +1,10 @@
 package com.programmersbox.fiveofakind
 
-import androidx.room.ConstructedBy
-import androidx.room.Dao
-import androidx.room.Database
-import androidx.room.Entity
-import androidx.room.Ignore
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.PrimaryKey
-import androidx.room.Query
-import androidx.room.RoomDatabase
-import androidx.room.RoomDatabaseConstructor
+import androidx.room.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.mapNotNull
 import kotlinx.datetime.Clock
 
 internal const val HIGHSCORE_LIMIT = 15
@@ -170,6 +158,7 @@ interface YahtzeeDao {
             yahtzeeScoreItem::smallStraight,
             yahtzeeScoreItem::largeStraight,
             yahtzeeScoreItem::chance,
+            yahtzeeScoreItem::yahtzee,
         )
 
         for(i in list) {
@@ -183,7 +172,7 @@ interface YahtzeeDao {
                         totalPoints = stat.totalPoints + i.get(),
                     )
                 } ?: YahtzeeScoreStat(
-                handType = i.name,
+                handType = i.name.replaceFirstChar { if (it.isLowerCase()) it.titlecase() else it.toString() },
                 numberOfTimes = 1,
                 totalPoints = i.get().toLong(),
             )
@@ -192,129 +181,6 @@ interface YahtzeeDao {
         }
     }
 }
-
-/*internal class YahtzeeDatabase(name: String = Realm.DEFAULT_FILE_NAME) {
-    private val realm by lazy {
-        Realm.open(
-            RealmConfiguration.Builder(
-                setOf(
-                    YahtzeeHighScores::class,
-                    YahtzeeScoreItem::class,
-                    YahtzeeScoreStat::class
-                )
-            )
-                .schemaVersion(27)
-                .name(name)
-                .migration({ })
-                //.deleteRealmIfMigrationNeeded()
-                .build()
-        )
-    }
-
-    private val yahtzeeHighScores: YahtzeeHighScores = realm.initDbBlocking { YahtzeeHighScores() }
-
-    suspend fun addHighScore(scoreItem: YahtzeeScoreItem) {
-        scoreItem.setScores()
-        realm.write {
-            copyToRealm(scoreItem)
-
-            val scores = query<YahtzeeScoreItem>()
-                .sort("totalScore", Sort.DESCENDING)
-                .find()
-
-            if (scores.size > HIGHSCORE_LIMIT) {
-                scores.chunked(HIGHSCORE_LIMIT)
-                    .drop(1)
-                    .flatten()
-                    .mapNotNull { findLatest(it) }
-                    .forEach { delete(it) }
-            }
-
-            query<YahtzeeHighScores>()
-                .first()
-                .find()
-                ?.let { updateScoreStats(it, scoreItem) }
-        }
-    }
-
-    private fun updateScoreStats(yahtzeeHighScores: YahtzeeHighScores, scoreItem: YahtzeeScoreItem) {
-        val scoreToStatMap = mapOf(
-            scoreItem::ones to yahtzeeHighScores::ones,
-            scoreItem::twos to yahtzeeHighScores::twos,
-            scoreItem::threes to yahtzeeHighScores::threes,
-            scoreItem::fours to yahtzeeHighScores::fours,
-            scoreItem::fives to yahtzeeHighScores::fives,
-            scoreItem::sixes to yahtzeeHighScores::sixes,
-            scoreItem::threeKind to yahtzeeHighScores::threeKind,
-            scoreItem::fourKind to yahtzeeHighScores::fourKind,
-            scoreItem::fullHouse to yahtzeeHighScores::fullHouse,
-            scoreItem::smallStraight to yahtzeeHighScores::smallStraight,
-            scoreItem::largeStraight to yahtzeeHighScores::largeStraight,
-            scoreItem::chance to yahtzeeHighScores::chance,
-        )
-
-        for ((scoreProperty, statProperty) in scoreToStatMap) {
-            statProperty.get()?.let { updateStatIfNonZero(scoreProperty.get(), it) }
-        }
-
-        if (yahtzeeHighScores.yahtzee != null) {
-            if (scoreItem.yahtzee != 0) {
-                val yahtzeeScore = (scoreItem.yahtzee - 50) / 100
-                yahtzeeHighScores.yahtzee!!.numberOfTimes++
-                repeat(yahtzeeScore) {
-                    yahtzeeHighScores.yahtzee!!.numberOfTimes++
-                }
-                yahtzeeHighScores.yahtzee!!.totalPoints += scoreItem.yahtzee
-            }
-        }
-    }
-
-    private fun updateStatIfNonZero(score: Int, stat: YahtzeeScoreStat) {
-        if (score != 0) {
-            stat.numberOfTimes++
-            stat.totalPoints += score
-        }
-    }
-
-    suspend fun removeHighScore(scoreItem: YahtzeeScoreItem) {
-        realm.write { findLatest(scoreItem)?.let { delete(it) } }
-    }
-
-    fun getYahtzeeHighScores() = realm.query<YahtzeeScoreItem>()
-        .sort("totalScore", Sort.DESCENDING)
-        .asFlow()
-        .mapNotNull { it.list }
-
-    fun getYahtzeeStats() = yahtzeeHighScores
-        .asFlow()
-        .mapNotNull { it.obj }
-
-    suspend fun updateScores() {
-        realm.write {
-            realm.query<YahtzeeScoreItem>()
-                .sort("totalScore", Sort.DESCENDING)
-                .find()
-                .forEach { it.setScores() }
-        }
-    }
-}*/
-
-/*internal class YahtzeeHighScores : RealmObject {
-    var ones: YahtzeeScoreStat? = YahtzeeScoreStat()
-    var twos: YahtzeeScoreStat? = YahtzeeScoreStat()
-    var threes: YahtzeeScoreStat? = YahtzeeScoreStat()
-    var fours: YahtzeeScoreStat? = YahtzeeScoreStat()
-    var fives: YahtzeeScoreStat? = YahtzeeScoreStat()
-    var sixes: YahtzeeScoreStat? = YahtzeeScoreStat()
-
-    var threeKind: YahtzeeScoreStat? = YahtzeeScoreStat()
-    var fourKind: YahtzeeScoreStat? = YahtzeeScoreStat()
-    var fullHouse: YahtzeeScoreStat? = YahtzeeScoreStat()
-    var smallStraight: YahtzeeScoreStat? = YahtzeeScoreStat()
-    var largeStraight: YahtzeeScoreStat? = YahtzeeScoreStat()
-    var yahtzee: YahtzeeScoreStat? = YahtzeeScoreStat()
-    var chance: YahtzeeScoreStat? = YahtzeeScoreStat()
-}*/
 
 @Entity(tableName = "YahtzeeScoreStat")
 data class YahtzeeScoreStat(
@@ -345,33 +211,3 @@ data class YahtzeeScoreItem(
     val largeScore: Int = threeKind + fourKind + fullHouse + smallStraight + largeStraight + yahtzee + chance,
     val totalScore: Int = largeScore + smallScore + if (smallScore >= 63) 35 else 0,
 )
-
-/*
-fun RealmInstant.toInstant(): Instant {
-    val sec: Long = this.epochSeconds
-    // The value always lies in the range `-999_999_999..999_999_999`.
-    // minus for timestamps before epoch, positive for after
-    val nano: Int = this.nanosecondsOfSecond
-    return if (sec >= 0) { // For positive timestamps, conversion can happen directly
-        Instant.fromEpochSeconds(sec, nano.toLong())
-    } else {
-        // For negative timestamps, RealmInstant starts from the higher value with negative
-        // nanoseconds, while Instant starts from the lower value with positive nanoseconds
-        // TODO This probably breaks at edge cases like MIN/MAX
-        Instant.fromEpochSeconds(sec - 1, 1_000_000 + nano.toLong())
-    }
-}
-
-fun Instant.toRealmInstant(): RealmInstant {
-    val sec: Long = this.epochSeconds
-    // The value is always positive and lies in the range `0..999_999_999`.
-    val nano: Int = this.nanosecondsOfSecond
-    return if (sec >= 0) { // For positive timestamps, conversion can happen directly
-        RealmInstant.from(sec, nano)
-    } else {
-        // For negative timestamps, RealmInstant starts from the higher value with negative
-        // nanoseconds, while Instant starts from the lower value with positive nanoseconds
-        // TODO This probably breaks at edge cases like MIN/MAX
-        RealmInstant.from(sec + 1, -1_000_000 + nano)
-    }
-}*/
